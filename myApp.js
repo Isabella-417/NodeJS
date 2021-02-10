74eb967c5270b72fdbd555eaf166221c5d653a41
Isabella Serna <isabella.serna.ramirez@gmail.com>
2021-02-10 12:17:15 -0500

    fix: return json object

--- myApp.js
+++ myApp.js
@@ -2,19 +2,19 @@ var express = require("express");
 var app = express();
 require("dotenv").config();
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.get("/", (req, res) => {
   const absolutePath = `${__dirname}/views/index.html`;
   res.sendFile(absolutePath);
 });
 
-
 app.get("/json", (req, res) => {
   let obj = { message: "Hello json" };
-  const message =
-    process.env.MESSAGE_STYLE === "uppercase" ? obj.message.toUpperCase() : obj;
-  res.json(message);
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    obj.message = obj.message.toUpperCase();
+  }
+  res.json(obj);
 });
 
 module.exports = app;
